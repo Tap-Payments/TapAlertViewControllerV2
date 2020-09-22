cde656f833108398c2640ac622a02c69c1ad5341
O Rabie <o.rabie@tap.company>
2020-09-22 11:19:29 +0200

    Upgrade to Xcode12+ compiler code friendly

---
+++ TapAlertViewControllerV2/TapAlertViewControllerV2/TapAlertViewControllerV2.h
@@ -0,0 +1,18 @@
+//
+//  TapAlertViewControllerV2.h
+//  TapAlertViewControllerV2
+//
+//  Created by Osama Rabie on 9/22/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TapAlertViewControllerV2.
+FOUNDATION_EXPORT double TapAlertViewControllerV2VersionNumber;
+
+//! Project version string for TapAlertViewControllerV2.
+FOUNDATION_EXPORT const unsigned char TapAlertViewControllerV2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapAlertViewControllerV2/PublicHeader.h>
+
+
